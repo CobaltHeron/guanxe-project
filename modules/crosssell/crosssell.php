c252c76d5ac90e3516bdfa6fe61b6460ba637d05
CobaltHeron <CobaltHeron@users.noreply.github.com>
2025-01-20 16:58:10 +0000

    Creado myproduct.tpl para testing de hookDisplayFooterProduct

--- modules/crosssell/crosssell.php
+++ modules/crosssell/crosssell.php
@@ -4,7 +4,6 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-
 class CrossSell extends Module
 {
     public function __construct()
@@ -25,7 +24,8 @@ class CrossSell extends Module
             return false;
         }
 
-        $this->registerHook('displayHome');
+        // $this->registerHook('displayHome');
+        $this->registerHook('displayFooterProduct');
 
         return true;
     }
@@ -38,6 +38,18 @@ class CrossSell extends Module
 
         return true;
     }
+
+
+    public function hookDisplayFooterProduct($params)
+    {
+        $this->context->smarty->assign('product_example', 'AQUI ES DONDE EL HOOK displayFooterProduct SE MUESTRA');
+        return $this->display(__FILE__, 'views/templates/front/myproduct.tpl');
+
+    }
+
+
+
+
 
     // LLAMADA A LA API AQUÍ ABAJO. DESCOMENTAR CUANDO LA TENGAMOS  (LINEA 45):
 
---
+++ modules/crosssell/views/templates/front/myproduct.tpl
@@ -0,0 +1,4 @@
+<div>
+    <p>{$product_example}</p>
+    <p>Texto de ejemplo. De momento todo bien :)</p>
+</div>
